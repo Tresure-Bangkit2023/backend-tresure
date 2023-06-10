47f4e9576c159af71c0b4193f252b0c6b98caf18
inlandsche <qolandara@gmail.com>
2023-06-10 17:49:24 +0700

    add status login

--- routes/users.js
+++ routes/users.js
@@ -75,7 +75,7 @@ router.post('/login', async (req, res) => {
 
     const token = jwt.sign({ id: user.id, username: user.username }, `${process.env.JWT_SECRET_KEY}`, { expiresIn: '1d' });
 
-    res.json({ 
+    res.status(200).json({ 
       message: 'Login success!',
       token });
   } catch (error) {

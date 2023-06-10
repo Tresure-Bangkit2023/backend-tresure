abe6578dddf900677ed671d0a8a8e26347221429
inlandsche <qolandara@gmail.com>
2023-06-10 15:10:42 +0700

    edit port

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const userLikedRouter = require('./routes/userLikedCategory');
 const ratingRouter = require('./routes/ratings');
 
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 // Parse JSON request bodies
 app.use(express.json());

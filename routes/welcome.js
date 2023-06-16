1508ca6387ade545a3e91a047109da77e7e456af
inlandsche <qolandara@gmail.com>
2023-06-16 10:54:05 +0700

    [Add] root message

---
+++ routes/welcome.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+router.use(express.json());
+
+router.get('/', async(req, res) => {
+    try {
+        res.status(200).json({
+            error: false,
+            message: "Hello, if you get this message it's mean this service is running."
+        });
+    } catch (error) {
+        res.status(500).json({
+            error: true,
+            message: 'Internal Server Error.'
+        });
+    }
+});
+
+module.exports = router;
